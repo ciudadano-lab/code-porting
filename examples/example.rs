1ad6f5896518ad598421b32af1e1895ccebd11d3
Wertegved <jatin.sharma062007@gmail.com>
2026-08-02 19:21:50 +0530

    Delete examples directory

--- examples/example.rs
+++
@@ -1,7 +0,0 @@
-// Port of example.c
-
-fn main() {
-    let c = "sqrt(5^2+7^2+11^2+(8-2)^2)";
-    let r = tinyexpr::te_interp(c).unwrap_or(f64::NAN);
-    println!("The expression:\n\t{c}\nevaluates to:\n\t{r:.6}");
-}
